6689689ec17d500f397158db9ebeb17ba14f27df
doaamagdy2024 <71094316+doaamagdy2024@users.noreply.github.com>
2021-05-04 17:05:49 +0200

    some modifications in event classes

--- Cancellation.cpp
+++ Cancellation.cpp
@@ -7,17 +7,18 @@ Cancellation::Cancellation(int ed,int id, MarsStation* m_s):Event(ed,id, m_s)
 
 void Cancellation::Execute()   //cancel a requested mountainous mission (if found and is waiting)
 {
-	M_S->increment_Cancelled_M();
+	
 	int count = M_S->get_waiting_mountainous_missions_().getItemCount();
 	// loop on the list
 	for (int i = 1; i <= count; i++)
 	{
-		Node<Mission*>* m = M_S->get_waiting_mountainous_missions_().getNodeAt(i);
-		if (m->getItem()->getID() == get_ID() && m->getItem()->getMT() == MISSION_TYPE::MOUNTAINOUS)
+		Mission* m = M_S->get_waiting_mountainous_missions_().getEntry(i);
+		if (m->getID() == get_ID() && m->getMT() == MISSION_TYPE::MOUNTAINOUS)
 		{
-			m->getItem()->setMS(MISSION_STATUS::CANCELLED);
+			m->setMS(MISSION_STATUS::CANCELLED);
 			//then cancel it
 			M_S->get_waiting_mountainous_missions_().remove(i);   //function remove does not delete
+			M_S->increment_Cancelled_M();
 		}
 
 	}
